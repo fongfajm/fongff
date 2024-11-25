890037ea4deb156d7cac45d7c050f28efd743ae4
fongfajm <phongfajm@gmail.com>
2024-11-25 23:38:23 +0700

    first commit

---
+++ src/main/java/com/laptrinhjavaweb/controller/admin/HomeController.java
@@ -0,0 +1,22 @@
+package com.laptrinhjavaweb.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller(value ="homeControllerOfAdmin")
+public class HomeController {
+  
+   @RequestMapping(value = "/home", method = RequestMethod.GET)
+   public ModelAndView homePage() {
+      ModelAndView mav = new ModelAndView("admin/home");
+      return mav;
+   }
+  
+//   @RequestMapping(value = "/springmvc", method = RequestMethod.GET)
+//   public ModelAndView springMVCPage() {
+//      ModelAndView mav = new ModelAndView("springmvc");
+//      return mav;
+//   }
+}
